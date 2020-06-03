f278798146b0182e087b090ade793b0dbcac508d
Yanye Li <yanye.li@grabtaxi.com>
2020-06-03 16:21:18 +0800

    MinDistanceBetweenWords

--- algorithm/dp/2d.go
+++ algorithm/dp/2d.go
@@ -53,3 +53,55 @@ func countSquares(matrix [][]int) int {
 	}
 	return count
 }
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minDistance(word1 string, word2 string) int {
+	if len(word1) == 0 {
+		return len(word2)
+	}
+	if len(word2) == 0 {
+		return len(word1)
+	}
+
+	R, C := len(word1), len(word2)
+
+	dp := make([]int, (R+1)*(C+1))
+	set := func(r, c int, v int) {
+		dp[r*(C+1)+c] = v
+	}
+	get := func(r, c int) int {
+		return dp[r*(C+1)+c]
+	}
+
+	for r := 0; r <= R; r++ {
+		set(r, 0, r)
+	}
+	for c := 0; c <= C; c++ {
+		set(0, c, c)
+	}
+
+	for r := 1; r <= R; r++ {
+		cr := word1[r-1]
+		for c := 1; c <= C; c++ {
+			cc := word2[c-1]
+			v := 0
+			if cr == cc {
+				v = min(get(r-1, c-1), min(get(r-1, c), get(r, c-1))+1)
+			} else {
+				v = min(get(r-1, c-1), min(get(r-1, c), get(r, c-1))) + 1
+			}
+			set(r, c, v)
+		}
+	}
+	return get(R, C)
+}
+
+func MinDistance(word1 string, word2 string) int {
+	return minDistance(word1, word2)
+}
--- main/main.go
+++ main/main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/leetcode/algorithm"
+	"github.com/leetcode/algorithm/dp"
 )
 
 func main() {
 	//fmt.Println(algorithm.FindMaxLength([]int{0,0,0,0}))
-	fmt.Println(algorithm.CanFinish(6, [][]int{
-		{0, 1},
-		{0, 2},
-		{2, 3},
-		{3, 1},
-		{4, 0},
-		{5, 4},
-	}))
+	fmt.Println(dp.MinDistance("applelm", "ableme"))
 }
